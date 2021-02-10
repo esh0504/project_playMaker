40352a23b4bd2e6fd23aca87257e32395b6cd3a2
음승호(seung ho - eum) <39682743+esh0504@users.noreply.github.com>
2021-02-10 16:39:16 +0900

    카카오 로그인 구현

--- src/screen/home/Home.js
+++ src/screen/home/Home.js
@@ -3,11 +3,11 @@ import React from "react";
 import Header from "../../components/common/header";
 import MainContentsContainer from "../../components/common/main_contents_container";
 import SideContentsContainer from "../../components/common/side_contents_container";
-import { connect } from 'react-redux';
-import { actionMarkerclick } from '../../redux/actions/index';
+
 import "./Home.css";
 
 class Home extends React.Component {
+  
   componentDidMount() {
     // 전체 데이터 받아오기
     // 1. 매칭데이터
@@ -16,7 +16,6 @@ class Home extends React.Component {
   }
 
   render() {
-    {console.log(this.props)}
     return (
       <section className="container">
         <Header/>
@@ -29,19 +28,5 @@ class Home extends React.Component {
   }
 }
 
-let mapDispatchToProps = (dispatch) => {
-  return {
-      onMarkerClick: () => dispatch(actionMarkerclick()),
-  }
-}
-
-let mapStateToProps = (state) => {
-  return {
-      marker_clicked : state.marker_clicked
-  };
-}
-
-//mapStateToProps를 사용하여 컴포넌트를 store에 연결시킨다.
-Home = connect(mapStateToProps,mapDispatchToProps)(Home);
 
 export default Home;
