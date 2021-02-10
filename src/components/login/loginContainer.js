15cf29b8340fd585f35cfaf35ad2601cb08dfc99
음승호(seung ho - eum) <39682743+esh0504@users.noreply.github.com>
2021-02-10 17:07:42 +0900

    구글 로그인 구현

--- src/components/login/loginContainer.js
+++ src/components/login/loginContainer.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import KaKaoLogin from 'react-kakao-login';
+import KakaoSignUp from "./KaKaoSignUp";
+import GoogleSignUp from "./GoogleSignUp";
 
-
-class KakaoSignUp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: 'kakao'
-        }
-    }
-
-    responseKaKao = (res) => {
-        this.setState({
-            data: res
-        })
-        alert(JSON.stringify(this.state.data))
-    }
-
-    responseFail = (err) => {
-        alert(err);
-    }
+class LoginContainer extends Component {
+    
 
     render() {
         return (
             <>
                 <div>
-                    <br></br>
-                    <KaKaoBtn
-                        buttonText="KaKao"
-                        onSuccess={this.responseKaKao}
-                        onFailure={this.responseFail}
-                        getProfile={true}
-                    />
+                    <KakaoSignUp></KakaoSignUp>
+                    <GoogleSignUp></GoogleSignUp>
                 </div>
 
             </>
@@ -40,22 +18,5 @@ class KakaoSignUp extends Component {
     }
 }
 
-const KaKaoBtn = styled(KaKaoLogin)`
-    padding: 0;
-    width: 190px;
-    height: 44px;
-    line-height: 44px;
-    color: #783c00;
-    background-color: #FFEB00;
-    border: 1px solid transparent;
-    border-radius: 3px;
-    font-size: 16px;
-    font-weight: bold;
-    text-align: center;
-    cursor: pointer;
-    &:hover{
-        box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2)
-    }
-`
 
-export default KakaoSignUp;
+export default LoginContainer;
